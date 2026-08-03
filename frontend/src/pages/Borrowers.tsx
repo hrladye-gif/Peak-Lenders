97725669a94fec0f10a5e32153e613a160a7a84c
hrladye-gif <hr.ladye@gmail.com>
2026-08-03 10:22:52 +0300

    Update Borrowers.tsx

--- frontend/src/pages/Borrowers.tsx
+++ frontend/src/pages/Borrowers.tsx
@@ -280,7 +280,7 @@ export const Borrowers = () => {
   
           <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
   
-            <div className="bg-white p-8 rounded-3xl w-[500px] shadow-xl">
+            <div className="bg-white p-8 rounded-3xl w-[900px] max-h-[90vh] overflow-y-auto shadow-xl">
   
               {/* Stepper */}
   
